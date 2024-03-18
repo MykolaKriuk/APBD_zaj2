992626bd6fe8477bd68413d9442bb29fb8b54056
Mykola <kopernik134@Mykolas-MacBook-Air.local>
2024-03-18 16:05:20 +0100

    Added a method for finding average value

--- APBD_zaj2/APBD_zaj2/Program.cs
+++ APBD_zaj2/APBD_zaj2/Program.cs
@@ -3,3 +3,18 @@ Console.WriteLine("Hello World!");
 Console.WriteLine("Hello Modification 1!");
 Console.WriteLine("Hello Modification 2!");
 Console.WriteLine("Hello Modification 3!");
+
+Console.WriteLine(GetAverage([1,2,3,4,5]));
+
+return;
+
+static string GetAverage(int[] array)
+{
+    int sum = 0;
+    foreach (var i in array)
+    {
+        sum += i;
+    }
+
+    return "Average is " + (double)sum / array.Length;
+}
